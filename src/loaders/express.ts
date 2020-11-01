5674f8e5e2a3012ef6e7277b75bfedaaf3fc15a1
aksv <aksv.wrk@gmail.com>
2020-11-01 19:42:54 +0300

    add cors handling

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export default (app: Express) => {
+    app.use(cors());
     app.use(bodyParser.json({ type: 'application/json' }));
 };
